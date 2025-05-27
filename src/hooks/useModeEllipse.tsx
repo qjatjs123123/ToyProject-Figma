e9df0fcf3a6cb411739e84a5ee315329abaa4a27
홍범선 <qjatjs123123@naver.com>
2025-05-28 01:57:18 +0900

    feat: Ellipse 모드일 때 마우스 Move시 좌표 설정 구현

--- src/hooks/useModeEllipse.tsx
+++ src/hooks/useModeEllipse.tsx
@@ -1,6 +1,7 @@
 import type { KonvaEventObject } from "konva/lib/Node";
 import { useEffect, useRef, useState } from "react";
 import type { EllipseType } from "../type/Shape";
+import { useShapeRefState } from "../contexts/ShapeRefContext";
 
 interface PointProps {
   x: number;
@@ -13,10 +14,18 @@ export default function useModeEllipse() {
   ); // 공통 로직
   const isCreating = useRef(false); // 공통로직직
   const startPoint = useRef<PointProps>({ x: 0, y: 0 }); // 공통 로직
+  const {
+    drawingShapeRef,
+    rectRefs,
+    transformerRef,
+    selectedIds,
+    setSelectedIds,
+    setMode,
+  } = useShapeRefState();
 
   useEffect(() => {
-    console.log(creatingEllipse)
-  }, [creatingEllipse])
+    console.log(creatingEllipse);
+  }, [creatingEllipse]);
 
   const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
     if (e.target !== e.target.getStage()) return;
@@ -27,6 +36,7 @@ export default function useModeEllipse() {
     isCreating.current = true;
     startPoint.current = pos;
 
+    // 여기만 다르면 됨 그리는 도구마다 다름 인자로 받기기
     setCreatingEllipse({
       x: pos.x,
       y: pos.y,
@@ -37,14 +47,32 @@ export default function useModeEllipse() {
       type: "shape",
       fill: "#D9D9D9",
       rotation: 0,
-      stroke: 'black',
-      strokeWidth: 2
+      stroke: "black",
+      strokeWidth: 2,
     });
-    
+
+    // 모드 따라 다르게,
+    setSelectedIds([`Ellipse ${1}`]);
+  };
+
+  const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+    if (!isCreating.current || !startPoint.current || !creatingEllipse) return;
+
+    const pos = e.target.getStage()?.getPointerPosition() as EllipseType;
+
+    setCreatingEllipse({
+      ...creatingEllipse,
+      x: Math.min(startPoint.current.x, pos.x),
+      y: Math.min(startPoint.current.y, pos.y),
+      radiusX: Math.abs(pos.x - startPoint.current.x),
+      radiusY: Math.abs(pos.y - startPoint.current.y),
+    });
 
   };
 
+
   return {
     handleMouseDown,
+    handleMouseMove,
   };
 }
